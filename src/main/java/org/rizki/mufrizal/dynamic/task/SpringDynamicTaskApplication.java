f3012cc2fab7a1820a591c99b6a550cbda15a4a3
RizkiMufrizal <mufrizalrizki@gmail.com>
2023-02-27 21:11:32 +0700

    initial code

---
+++ src/main/java/org/rizki/mufrizal/dynamic/task/SpringDynamicTaskApplication.java
@@ -0,0 +1,13 @@
+package org.rizki.mufrizal.dynamic.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamicTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDynamicTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rizki/mufrizal/dynamic/task/SpringDynamicTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rizki.mufrizal.dynamic.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamicTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
